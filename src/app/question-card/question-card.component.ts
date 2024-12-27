918a85ea4c458fcca2e97f23847b75230c3ce7e2
santhoshlm10 <santhosh4u1997@gmail.com>
2024-12-27 08:40:07 +0530

    css and options encode

--- src/app/question-card/question-card.component.ts
+++ src/app/question-card/question-card.component.ts
@@ -53,7 +53,7 @@ export class QuestionCardComponent {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
-    return array;
+    return array.map((item) => this.decodeHtmlEntities(item));
   }
 
 
